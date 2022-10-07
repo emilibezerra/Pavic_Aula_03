2336f6cd70f3abbe2e86f0e642bca5015ebff44e
emilibezerra <emilisilva23@gmail.com>
2022-10-06 19:34:26 -0500

    Adicionar arquivos de projeto.

---
+++ Origem.cpp
@@ -0,0 +1,32 @@
+
+
+#include<iostream>
+#include<fstream> // Read and Write Images
+
+using namespace std;
+
+int main() {
+    // Create a PPM image
+    ofstream image;
+
+    image.open("./images/RGB.ppm");
+    if (image.is_open()) {
+        //Header
+        image << "P3" << endl; //Image type
+        image << "3 2" << endl; // Width and Height
+        image << "255" << endl; //Maximum value
+
+        //Image content
+        image << "255 0 0" << endl; //RED
+        image << "0 255 0" << endl; //GREEN
+        image << "0 0 255" << endl; //BLUE
+        image << "255 255 0" << endl;//YELLOW
+        image << "255 255 255" << endl; //WHITE
+        image << "0 0 0" << endl; //BLACK
+        image << "0 0 0" << endl; //BLACK
+        image << "0 0 0" << endl; //BLACK
+    }
+
+    image.close();
+
+}
